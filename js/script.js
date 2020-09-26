2f082d79a7050616c095d7e9fa5054ccdb1e52fd
kimamishka <66490635+kimamishka@users.noreply.github.com>
2020-09-26 13:02:24 +0300

    добавила слайдеры

--- js/script.js
+++ js/script.js
@@ -5,3 +5,91 @@ $(document).ready(function () {
       $("body").toggleClass("lock"); /*чтобы при открытом меню бургера текст под меню случайно не скролился, оставался на том же месте, где мы зашли в бургер*/
    });
 });
+
+//СЛАЙДЕР
+$(document).ready(function () {
+   $('.slider1').slick({
+      prevArrow: "<img src='img/left_arrow.svg' class='prev' alt='1'>",
+      nextArrow: "<img src='img/right_arrow.svg' class='next' alt='2'>",
+      arrows: true,
+      dots: false,
+      adaptiveHeight: true,
+      slidesToShow: 4,
+      autoplay: false,
+      speed: 1000,/*1000=1секунда*/
+      autoplaySpeed: 1400,
+      responsive: [
+         {
+            breakpoint: 980,
+            settings: {
+               prevArrow: "<img src='img/news/left-320.jpg' class='prev' alt='1'>",
+               nextArrow: "<img src='img/news/right-320.jpg' class='next' alt='2'>"
+            }
+         },
+         {
+            breakpoint: 950,
+            settings: {
+               slidesToShow: 3,
+               prevArrow: "<img src='img/news/left-320.jpg' class='prev' alt='1'>",
+               nextArrow: "<img src='img/news/right-320.jpg' class='next' alt='2'>"
+            }
+         },
+         {
+            breakpoint: 700,
+            settings: {
+               slidesToShow: 2,
+               prevArrow: "<img src='img/news/left-320.jpg' class='prev' alt='1'>",
+               nextArrow: "<img src='img/news/right-320.jpg' class='next' alt='2'>"
+            }
+         },
+         {
+            breakpoint: 500,
+            settings: {
+               slidesToShow: 1,
+               prevArrow: "<img src='img/news/left-320.jpg' class='prev' alt='1'>",
+               nextArrow: "<img src='img/news/right-320.jpg' class='next' alt='2'>"
+            }
+         }
+      ]
+   });
+});
+
+$(document).ready(function () {
+   $('.slider-brands').slick({
+      prevArrow: "<img src='img/news/left-320.jpg' class='prev' alt='1'>",
+      nextArrow: "<img src='img/news/right-320.jpg' class='next' alt='2'>",
+      arrows: true,
+      dots: false,
+      adaptiveHeight: true,
+      slidesToShow: 9,
+      autoplay: false,
+      speed: 1000,
+      autoplaySpeed: 1400,
+      responsive: [
+         {
+            breakpoint: 980,
+            settings: {
+               slidesToShow: 7,
+            }
+         },
+         {
+            breakpoint: 750,
+            settings: {
+               slidesToShow: 5,
+            }
+         },
+         {
+            breakpoint: 510,
+            settings: {
+               slidesToShow: 4,
+            }
+         },
+         {
+            breakpoint: 440,
+            settings: {
+               slidesToShow: 3,
+            }
+         }
+      ]
+   });
+});
